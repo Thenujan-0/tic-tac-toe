dbd722cf95b6e607d03ee5f2103d4b8cefd2f143
Thenujan Sandramohan <sthenujan2002@gmail.com>
2022-12-25 22:36:08 +0530

    Redesigned the ui

--- main.js
+++ main.js
@@ -1,17 +1,13 @@
-import './style.css'
-import './node_modules/bootstrap/dist/css/bootstrap.css'
-import './node_modules/bootstrap/dist/js/bootstrap.bundle'
+import './style.css';
+import 'bootstrap/dist/css/bootstrap.css';
+import 'bootstrap/dist/js/bootstrap.bundle';
 
 const testConfigs = {
   noOverlay: true,
 };
 
-let currentPlayerX = true;
+let currentPlayerX = false;
 let gameOver = false;
-
-function changeCurrPlayer() {
-  currentPlayerX = !currentPlayerX;
-}
 
 const converter = {
   inVw(width) {
@@ -32,6 +28,11 @@ function $(elem) {
   return document.querySelectorAll(elem);
 }
 
+function changeCurrPlayer() {
+  currentPlayerX = !currentPlayerX;
+  $('span#currentPlayer')[0].textContent = currentPlayerX ? 'X' : 'O';
+}
+
 const main = $('main')[0];
 const boxHeight = '12vw';
 
@@ -39,307 +40,369 @@ const boxHeight = '12vw';
 const winAnimationDuration = '0.4s';
 const winBoxDelay = '1s';
 
-const winHandler = (function () {
-  function processResizeListener(argFunction) {
-    function createTimeoutCaller(argFunc) {
-      let timeout = null;
-      function callArgFunction() {
-        if (timeout != null) {
-          return;
-        }
-        setTimeout(() => {
-          argFunc();
-          timeout = null;
-        }, 50);
+const overlay = (function () {
+  const elem = $('.overlay')[0];
+
+  /** Shows the darkoverlay if testConfig.noOverlay is false */
+  function show() {
+    elem.style.display = testConfigs.noOverlay ? 'none' : 'block';
+  }
+  return { show };
+}());
+
+const scoreBoard = (function () {
+  const playerX = $('.player-x-scoreboard')[0];
+  const playerO = $('.player-o-scoreboard')[0];
+  const playerXName = playerX.querySelector('.playerX.name');
+  const playerOName = playerO.querySelector('.playerO.name');
+
+  function playAgainBtnCallback() {
+    board.reset();
+    reset();
+  }
+
+  function expand() {
+    const oBoard = scoreBoard.playerO;
+    const xBoard = scoreBoard.playerX;
+    const { left } = oBoard.getBoundingClientRect();
+    const { right } = xBoard.getBoundingClientRect();
+
+    const expandableDiv = currentPlayerX ? playerXName : playerOName;
+
+    // To animate
+    const currentWidth = Number.parseFloat(window.getComputedStyle(expandableDiv).width);
+    expandableDiv.style.width = `${currentWidth}px`;
+    const transitionTimeSeconds = '3';
+    expandableDiv.style.transition = `all ${transitionTimeSeconds}s`;
+    // addBtn();
+
+    setTimeout(() => {
+      expandableDiv.style.position = 'absolute';
+
+      if (currentPlayerX) {
+        expandableDiv.style.right = `${window.innerWidth - right}px`;
+      } else {
+        expandableDiv.style.left = `${left}px`;
       }
-      return callArgFunction;
+      expandableDiv.style.width = `${right - left}px`;
+
+      const textElem = expandableDiv.querySelector('p');
+      setTimeout(() => {
+        textElem.textContent = currentPlayerX ? 'Player X won' : 'Player O won';
+      }, (transitionTimeSeconds * 1000) / 2);
+      // addBtn();
+      const btn = $('.btn-playAgain');
+      btn.addEventListener('click', playAgainBtnCallback);
+    }, 500);
+  }
+
+  function handleWin() {
+    expand();
+    const btn = $('.btn-playAgain')[0];
+    btn.addEventListener('click', playAgainBtnCallback);
+  }
+
+  function animateWin() {
+    expand();
+  }
+
+  function reset() {
+    const expandableDiv = currentPlayerX ? playerXName : playerOName;
+    expandableDiv.style.removeProperty('position');
+    expandableDiv.style.removeProperty('left');
+    expandableDiv.style.removeProperty('width');
+
+    const textElem = expandableDiv.querySelector('p');
+    textElem.textContent = currentPlayerX ? 'Player X' : 'Player O';
+  }
+
+  return {
+    playerX, playerO, playerOName, playerXName, expand,
+  };
+}());
+
+const winBox = (function () {
+  const winBoxElem = $('.win')[0];
+  const winText = $('.win > p')[0];
+
+  function show(draw) {
+    winBoxElem.style.display = 'block';
+    winBoxElem.style.height = 'fit-content';
+    const realHeight = window.getComputedStyle(winBoxElem).height;
+    winBoxElem.style.height = '1px';
+
+    // animation duration in seconds
+    const animationDuration = 0.5;
+    winBoxElem.style.transition = `all ${animationDuration}s`;
+    const playAgainBtn = winBoxElem.querySelector('button.btn');
+
+    if (draw) {
+      winBoxElem.style.background = 'var(--draw-color)';
+      playAgainBtn.style.setProperty('background-color', 'var(--draw-btn-color)', 'important');
+    } else if (currentPlayerX) {
+      winBoxElem.style.background = 'var(--player-x-color)';
+      playAgainBtn.style.setProperty('background-color', 'var(--player-x-btn-color)', 'important');
+    } else {
+      winBoxElem.style.background = 'var(--player-o-color)';
+      playAgainBtn.style.setProperty('background-color', 'var(--player-o-btn-color)', 'important');
     }
 
-    const timedArgFunction = createTimeoutCaller(argFunction);
-    window.addEventListener('resize', timedArgFunction);
+    setTimeout(() => {
+      winBoxElem.style.height = realHeight;
+      winBoxElem.style.opacity = '100%';
+    }, 200);
 
-    timedArgFunction();
+    overlay.show();
   }
+
+  function hide() {
+    winBoxElem.style.setProperty('display', 'none', 'important');
+  }
+
+  function setText(text) {
+    winText.textContent = text;
+  }
+
+  return { show, setText, hide };
+}());
+
+const winHandler = (function () {
   function horizontalHandler(ind) {
     /** Returns Number value */
     function calculateLeft() {
-      return 50 - 1.25 * Number.parseInt(boxHeight, 3);
+      return 50 - 1.25 * Number.parseInt(boxHeight, 10);
     }
 
     const bar = $('#horizontalWin')[0];
-    bar.style.display = 'block';
 
     function alignHorizontalBar() {
-      bar.style.setProperty('top', `calc(50% - ${Number.parseInt(boxHeight, 3) * (1 - ind)}vw )`, 'important')
+      bar.style.setProperty('top', `calc(50% - ${Number.parseInt(boxHeight, 10) * (1 - ind)}vw )`, 'important');
       bar.style.setProperty('left', `${calculateLeft()}vw`, 'important');
     }
+    alignHorizontalBar();
+    bar.style.display = 'block';
+  }
 
-    processResizeListener(alignHorizontalBar);
+  function setWinBarColor() {
+    const bars = $('.winbar');
+    bars.forEach((bar) => {
+      if (currentPlayerX) {
+        bar.style.background = 'var(--player-x-color)';
+      } else {
+        bar.style.background = 'var(--player-o-color)';
+      }
+    });
   }
 
   function verticalHandler(ind) {
     const bar = $('#verticalWin')[0];
-    bar.style.display = 'block';
     const mainCenter = main.offsetWidth / 2;
     const mainCenterVw = converter.inVw(mainCenter);
 
-    const currPosition = mainCenterVw - (1 - ind) * Number.parseInt(boxHeight, 3);
+    const currPosition = mainCenterVw - (1 - ind) * Number.parseInt(boxHeight, 10);
     bar.style.setProperty('left', `${currPosition}vw`, 'important');
-    // let boxHeightpx = converter.vwToPx(Number.parseInt(boxHeight))
-
-    bar.style.setProperty('top',`calc(50% - ${Number.parseInt(boxHeight, 3) * 1.25}vw)`,'important')
+    // let boxHeightpx = converter.vwToPx(Number.parseInt(boxHeight, 10))
+    bar.style.setProperty('top', `calc(50% - ${Number.parseInt(boxHeight, 10) * 1.25}vw)`, 'important');
+    bar.style.display = 'block';
   }
 
-  function mainDiagonalHandler(){
-    function calculateTop(){
-      let boardCenter = main.offsetHeight/2   
-      let val =boardCenter  -1.25* converter.vwToPx(Number.parseInt(boxHeight))
-      console.log({val})
-      return val
-    }
-
-    function calculateLeft(){
-      let boardCenter = main.offsetWidth/2
-      let val = boardCenter - 1.5 * converter.vwToPx(Number.parseInt(boxHeight))
-      console.log({val})
-      return val
-    }
-    console.log(calculateTop(),'calculateTop')
-
-    let bar=$('#mainDiagonalWin')[0]
-    bar.style.display='block'
-
-    bar.style.setProperty('left',`calc(50% - ${Number.parseInt(boxHeight)*1.5}vw)`,'important')
-    bar.style.setProperty('top',`calc(50% - ${Number.parseInt(boxHeight)*1.25}vw)`,'important')
+  function mainDiagonalHandler() {
+    const bar = $('#mainDiagonalWin')[0];
+    bar.style.setProperty('left', `calc(50% - ${Number.parseInt(boxHeight, 10) * 1.5}vw)`, 'important');
+    bar.style.setProperty('top', `calc(50% - ${Number.parseInt(boxHeight, 10) * 1.25}vw)`, 'important');
+    bar.style.display = 'block';
   }
 
-  function check(data){
-    let win = false
-    data.forEach((row,ind)=>{
-      //check row win
-      if (!row.includes('') && row.every((elem)=>{return elem==row[0]})){
-        console.log('win by row')
-        horizontalHandler(ind)
-        win =  true
-        return true;
+  function secondDiagonalHandler() {
+    const bar = $('#secondDiagonalWin')[0];
+    bar.style.setProperty('left', `calc(50% + ${Number.parseInt(boxHeight, 10) * 1}vw)`, 'important');
+    bar.style.setProperty('top', `calc(50% - ${Number.parseInt(boxHeight, 10) * 1.25}vw)`, 'important');
+    bar.style.display = 'block';
+  }
+  // secondDiagonalHandler();
+
+  function check(data) {
+    let win = false;
+    data.forEach((row, ind) => {
+      //  We don't want to return true in each cases because a win might be in two scenerios at once
+
+      // check row win
+      if (!row.includes('') && row.every((elem) => elem === row[0])) {
+        horizontalHandler(ind);
+        win = true;
       }
 
-      //column win
-      if (data.every((row)=>row[ind]!='' && row[ind]===data[0][ind])){
-        console.log('win by column')
-        verticalHandler(ind)
+      // column win
+      if (data.every((row_) => row_[ind] !== '' && row_[ind] === data[0][ind])) {
+        verticalHandler(ind);
 
-        win = true
+        win = true;
       }
-    })
+    });
 
-    //win by main diaglonal
-    if(data.every((row,i)=>row[i]!='' && row[i]==data[0][0])){
-      mainDiagonalHandler()
-      win= true
+    // win by main diaglonal
+    if (data.every((row, i) => row[i] !== '' && row[i] === data[0][0])) {
+      mainDiagonalHandler();
+      win = true;
     }
 
-    //win by second diaglonal
-    if(data.every((row,i)=> row[2-i]!='' && row[2-i]===data[0][2])){
-      console.log('win by second diagonal')
-      win= true
+    // win by second diaglonal
+    if (data.every((row, i) => row[2 - i] !== '' && row[2 - i] === data[0][2])) {
+      secondDiagonalHandler();
+      win = true;
     }
 
-    if(win){
-      console.log('------------------------------------------')
-      gameOver = true
+    if (win) {
+      console.log('------------------------------------------');
+      gameOver = true;
     }
     return win;
   }
 
-  function handle(){
+  function handle() {
+    // Leave time for winBarAnimation
+    // setTimeout(winBox.show, Number.parseInt(winAnimationDuration, 10)
+    //  + Number.parseInt(winBoxDelay, 10) * 1000);
 
-    //Leave time for winBarAnimation
-    setTimeout(winBox.show,Number.parseInt(winAnimationDuration)+Number.parseInt(winBoxDelay)*1000)
+    if (currentPlayerX) {
+      winBox.setText('Player X won the game');
+      const playerXScore = $('.playerX.score')[0];
+      playerXScore.textContent = Number.parseInt(playerXScore.textContent, 10) + 1;
+    } else {
+      winBox.setText('Player O won the game');
+      const playerOScore = $('.playerO.score')[0];
+      playerOScore.textContent = Number.parseInt(playerOScore.textContent, 10) + 1;
+    }
 
-    if(currentPlayerX){
-      winBox.setText('Player X won the game')
-    }else{
-      winBox.setText('Player O won the game')
+    setWinBarColor();
+    scoreBoard.expand();
+  }
+
+  return {
+    horizontalHandler, verticalHandler, check, handle,
+  };
+}());
+
+const board = (function () {
+  const data = new Array(3);
+
+  function resetBoardData() {
+    for (let i = 0; i <= 2; i++) {
+      data[i] = new Array(3).fill('');
     }
   }
 
-
-
-
-  return {horizontalHandler,verticalHandler,check,handle}
-}())
-
-let overlay= (function(){
-  let elem = $('.overlay')[0]
-
-  /**Shows the darkoverlay if testConfig.noOverlay is false */
-  function show(){
-    elem.style.display=testConfigs.noOverlay ? 'none' : 'block'
-  }
-  return {show}
-}())
-
-let winBox = (function() {
-  const winBoxElem = $('.win')[0];
-  const winText = $('.win > p')[0];
-
-
-  function show(){
-
-    winBoxElem.style.display='block'
-    winBoxElem.style.height='fit-content'
-    let realHeight = window.getComputedStyle(winBoxElem).height
-    winBoxElem.style.height='1px'
-
-    //animation duration in seconds
-    let animationDuration=0.5
-    winBoxElem.style.transition='all '+animationDuration+'s'
-    
-    // let intervalCaller = setInterval(()=>{
-    //   moveListeners.forEach((listener)=>{
-    //     console.log('each listener is being called')
-    //     listener()
-    //   })
-    // },1)
-
-    // //Stop interval call once animation is finished
-    // setTimeout(()=>{
-    //   clearInterval(intervalCaller)
-    // },animationDuration*1000)
-
-    setTimeout(()=>{
-      winBoxElem.style.height=realHeight
-      winBoxElem.style.opacity='100%'
-    },200)
-
-    console.log(realHeight)
-    overlay.show()
-  }
-
-  function setText(text){
-      winText.textContent=text
-  }
-
-  return {show,setText}
-}())
-
-const board = (function() {
-  let data= new Array(3)
-
-
-
-  for(let i =0; i<=2;i++){
-    data[i]= new Array(3).fill('')
-  }
-
-  /**Used to print values of objects */
-  function parse(dict){
-    return JSON.parse(JSON.stringify(dict))
-  }
+  /** Used to print values of objects */
+  // function parse(dict) {
+  //   return JSON.parse(JSON.stringify(dict));
+  // }
   function drawInitial() {
-    let board = $('main')[0]
-    const borderColor='#747474'
+    const boardElem = $('main')[0];
+    const borderColor = '#747474';
 
-    //height and width of the boxes
-    
-   $(':root')[0].style.setProperty('--height',boxHeight);
-    $(':root')[0].style.setProperty('--winAnimationDuration',winAnimationDuration);
+    resetBoardData();
 
-    for (let i =0 ; i<=2;i++) {
-      let rowElemStart =`<div class='d-flex' data-row-id=${i}>`
-      let rowElemEnd ='</div>'
-      let boardInsert=''
-      boardInsert+=rowElemStart
+    // height and width of the boxes
+    $(':root')[0].style.setProperty('--height', boxHeight);
 
-      let rowBorders=['border-bottom','','border-top']
-      for (let j =0;j<=2;j++){
-        let columnBorders =['border-end','','border-start']
-        let columnElem = `<div class="box ${columnBorders[j]} ${rowBorders[i]} border-3 icon d-flex align-items-center justify-content-center" data-id-row=${i} data-id-col=${j} style='--bs-border-color:${borderColor}'></div>`
-        boardInsert+=columnElem
+    $(':root')[0].style.setProperty('--winAnimationDuration', winAnimationDuration);
+
+    for (let i = 0; i <= 2; i++) {
+      const rowElemStart = `<div class='d-flex' data-row-id=${i}>`;
+      const rowElemEnd = '</div>';
+      let boardInsert = '';
+      boardInsert += rowElemStart;
+
+      const rowBorders = ['border-bottom', '', 'border-top'];
+      for (let j = 0; j <= 2; j++) {
+        const columnBorders = ['border-end', '', 'border-start'];
+        const columnElem = `<div class="box ${columnBorders[j]} ${rowBorders[i]} border-3 icon d-flex align-items-center justify-content-center" data-id-row=${i} data-id-col=${j} style='--bs-border-color:${borderColor}'></div>`;
+        boardInsert += columnElem;
       }
-      boardInsert+=rowElemEnd
-      board.insertAdjacentHTML("beforeend",boardInsert)
-    }
-
-  }
-
-  function alreadySet(row,col){
-    if (data[row][col]!=""){
-      return true
-    }
-    return false
-  }
-  function boardFilled(){
-    return data.every((row)=>{
-      return row.every((val)=>{
-        return val!=""
-      })
-    })
-  }
-
-
-  function changeData(row,col){
-    if(currentPlayerX){
-      data[row][col]="X"
-    }else{
-      data[row][col]="O"
+      boardInsert += rowElemEnd;
+      boardElem.insertAdjacentHTML('beforeend', boardInsert);
     }
   }
 
-  function drawClick(elem){
-    if(currentPlayerX){
-      elem.textContent="X"
-      elem.classList.add("playerX")
-    }else{
-      elem.textContent="O"
-      elem.classList.add("playerO")
+  function reset() {
+    resetBoardData();
+    winBox.hide();
+    const winBars = $('.winbar');
+    winBars.forEach((bar) => { bar.style.display = 'none'; });
+    const boxes = $('.box');
+    boxes.forEach((box) => {
+      box.classList.remove('playerX');
+      box.classList.remove('playerO');
+      box.textContent = '';
+    });
+    gameOver = false;
+  }
+
+  function alreadySet(row, col) {
+    if (data[row][col] !== '') {
+      console.log('already set on ', row, col);
+      return true;
+    }
+    return false;
+  }
+  function boardFilled() {
+    return data.every((row) => row.every((val) => val !== ''));
+  }
+
+  function changeData(row, col) {
+    if (currentPlayerX) {
+      data[row][col] = 'X';
+    } else {
+      data[row][col] = 'O';
     }
   }
 
-  function handleClick(row,col){
-    if (alreadySet(row,col)|| gameOver){
-      return
+  function drawClick(elem) {
+    if (currentPlayerX) {
+      elem.textContent = 'X';
+      elem.classList.add('playerX');
+    } else {
+      elem.textContent = 'O';
+      elem.classList.add('playerO');
     }
-    changeCurrPlayer()
-
-    let elem = $(`div[data-id-row="${row}"][data-id-col="${col}"]`)[0]
-    drawClick(elem)
-    changeData(row,col)
-    if(winHandler.check(data)){
-      winHandler.handle()
-      return
-    }
-    if(boardFilled()){
-      winBox.show()
-      winBox.setText("Draw")
-    }
-
   }
 
+  function handleClick(row, col) {
+    if (alreadySet(row, col) || gameOver) {
+      return;
+    }
 
-  return {drawInitial,handleClick}
-}())
+    const elem = $(`div[data-id-row="${row}"][data-id-col="${col}"]`)[0];
+    drawClick(elem);
+    changeData(row, col);
+    if (winHandler.check(data)) {
+      winHandler.handle();
+      return;
+    }
+    if (boardFilled()) {
+      winBox.show(true);
+      winBox.setText('Draw');
+    }
 
+    changeCurrPlayer();
+  }
 
+  return { drawInitial, handleClick, reset };
+}());
 
-board.drawInitial()
+board.drawInitial();
 
-
-
-function clickCallback(e){
-  let row = this.getAttribute("data-id-row")
-  let col = this.getAttribute("data-id-col")
-  board.handleClick(row,col)
-
+function clickCallback() {
+  const row = this.getAttribute('data-id-row');
+  const col = this.getAttribute('data-id-col');
+  board.handleClick(row, col);
 }
 
+$('.box').forEach((elem) => {
+  elem.addEventListener('click', clickCallback);
+});
 
-
-$(".box").forEach(function(elem){
-  elem.addEventListener("click",clickCallback)
-})
-
-$("button")[0].addEventListener("click",function(){
-  window.location.reload()
-})
+$('button')[0].addEventListener('click', () => {
+  // window.location.reload();
+  board.reset();
+});
